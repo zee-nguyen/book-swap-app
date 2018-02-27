28066f02aae36203bd67cb2c50a0244bbde156b1
Zi Nguyen <zinguyenn@gmail.com>
2018-02-27 16:54:55 -0500

    add user login and sign up logic

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var express = require('express'),
+var express = require("express"),
 		app = express(),
-		mongoose = require('mongoose')
-		bodyParser = require('body-parser'),
-		methodOverride = require('method-override'),
-		Book = require('./models/book'),
-		Genre = require('./models/genre'),
-		seedDB = require('./seeds');
+		mongoose = require("mongoose"),
+		passport = require("passport"),
+		LocalStrategy = require("passport-local"),
+		passportLocalMongoose = require("passport-local-mongoose"),
+		expressSession = require("express-session"),
+		bodyParser = require("body-parser"),
+		methodOverride = require("method-override"),
+		Book = require("./models/book"),
+		User = require("./models/user"),
+		seedDB = require("./seeds");
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -21,6 +25,21 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // seedDB();
+
+// Express Session
+app.use(expressSession({
+	secret: "It's snowing today",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Config
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //Landing page
 app.get('/', function(req, res) {
@@ -96,6 +115,62 @@ app.put('/books/:id', function(req, res) {
 		res.redirect('/books');
 	})
 });
+
+//DELETE - delete book
+app.delete('/books/:id', function(req, res) {
+	//look up by id and delete
+	Book.findByIdAndRemove(req.params.id)
+	.then(function(removedBook) {
+		console.log('book removed!');
+		res.redirect('/books');
+	})
+	.catch(function(err) {
+		console.log(err);
+	})
+});
+
+/* ==========USER LOGIC===============*/
+//GET register form
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+//Handle user sign up
+app.post('/register', function(req, res) {
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user) {
+		if (err) {
+			console.log(err);
+		}
+		passport.authenticate("local")(req, res, function() {
+			console.log(user);
+			res.redirect('/books');
+		})
+	})
+});
+
+
+//GET login form
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+//Handle user login
+app.post('/login', function(req, res, next) {
+	passport.authenticate("local", function(err, user, info) {
+		if (err) { return next(err); }
+		if (!user) { return res.redirect('/login'); }
+		req.login(user, function(err) {
+			if (err) { return next(err); }
+			// var redirectTo = req.session.redirectTo ? req.session.redirectTo : "/books";
+			// delete req.session.redirectTo;
+			//
+			// res.redirect(redirectTo);
+			return res.redirect("/books");
+		})
+	})(req, res, next);
+});
+
 
 
 app.listen('8888', () => console.log('magic seriously is happening on port 8888'));
