7dd0a3385cec6781c48fce62ae5f19ba3acff325
Zi Nguyen <zinguyenn@gmail.com>
2018-02-27 15:45:03 -0500

    add update route for each book

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express'),
 		app = express(),
 		mongoose = require('mongoose')
 		bodyParser = require('body-parser'),
+		methodOverride = require('method-override'),
 		Book = require('./models/book'),
+		Genre = require('./models/genre'),
 		seedDB = require('./seeds');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //Setup Mongoose connection
 var dev_db_url = 'mongodb://admin:123adminbookswap123@ds041678.mlab.com:41678/book-swap';
@@ -28,8 +31,6 @@ app.get('/', function(req, res) {
 app.get('/books', function(req, res) {
 	//get all books from db
 	Book.find({})
-	.populate("author")
-	.populate("genre")
 	.then(function(allBooks) {
 		res.render('books/books', {books: allBooks});
 	})
@@ -45,17 +46,15 @@ app.get('/books/new', function(req, res) {
 
 //CREATE - post new book to db
 app.post('/books', function(req, res) {
-	//get data from form and add to book array
-	// TODO - fix issue where input needs to convert to id for genre and author
-	var book = req.body.book;
-	Book.create(book)
+	Book.create(req.body.book)
 	.then(function(newBook) {
-		console.log("new book created!");
+		console.log('new book created!');
+		console.log(newBook);
 		res.redirect('/books');
 	})
 	.catch(function(err) {
 		console.log(err);
-	});
+	})
 });
 
 //SHOW - get detail of each book to show
@@ -63,8 +62,6 @@ app.get('/books/:id', function(req, res) {
 	//look up book by id
 	Book.findById(req.params.id)
 	.populate('comments')
-	.populate('author')
-	.populate('genre')
 	.exec(function(err, foundBook) {
 		if (err) {
 			console.log(err);
@@ -75,7 +72,30 @@ app.get('/books/:id', function(req, res) {
 	});
 });
 
-//EDIT - edit book detail
+//EDIT - get edit form
+app.get('/books/:id/edit', function(req, res) {
+	//find book by ID and send data along to edit form
+	Book.findById(req.params.id)
+	.then(function(foundBook) {
+		res.render('books/edit', {book: foundBook});
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
+});
+
+//UPDATE - put edit info into database
+app.put('/books/:id', function(req, res) {
+	//look up book by id and update
+	Book.findByIdAndUpdate(req.params.id, req.body.book)
+	.then(function(updatedBook) {
+		res.redirect('/books/' + req.params.id);
+	})
+	.catch(function(err) {
+		console.log(err);
+		res.redirect('/books');
+	})
+});
 
 
 app.listen('8888', () => console.log('magic seriously is happening on port 8888'));
