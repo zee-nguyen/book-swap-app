faac0b1fa08a3203feaef2859f332eba8735d76a
Zi Nguyen <zinguyenn@gmail.com>
2018-02-28 15:10:07 -0500

    update comment logic

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ var express = require("express"),
 		passport = require("passport"),
 		LocalStrategy = require("passport-local"),
 		passportLocalMongoose = require("passport-local-mongoose"),
-		expressSession = require("express-session"),
+		// session = require("express-session"),
 		bodyParser = require("body-parser"),
 		methodOverride = require("method-override"),
+		favicon = require("serve-favicon"),
+		flash = require("flash"),
 		Book = require("./models/book"),
 		User = require("./models/user"),
 		seedDB = require("./seeds");
-
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride("_method"));
 
 //Setup Mongoose connection
 var dev_db_url = 'mongodb://admin:123adminbookswap123@ds041678.mlab.com:41678/book-swap';
@@ -24,13 +21,20 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+// app.use(flash());
+
 // seedDB();
 
-// Express Session
-app.use(expressSession({
-	secret: "It's snowing today",
-  resave: false,
-  saveUninitialized: false
+// Passport configuration
+app.use(require("express-session")({
+    secret: "It's snowing today",
+    resave: false,
+    saveUninitialized: false
 }));
 
 // Passport Config
@@ -40,6 +44,14 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//custom middleware
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	next();
+});
+
+
 
 //Landing page
 app.get('/', function(req, res) {
@@ -164,12 +176,114 @@ app.post('/login', function(req, res, next) {
 			if (err) { return next(err); }
 			// var redirectTo = req.session.redirectTo ? req.session.redirectTo : "/books";
 			// delete req.session.redirectTo;
-			//
 			// res.redirect(redirectTo);
 			return res.redirect("/books");
 		})
 	})(req, res, next);
 });
+
+//Logout
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+/* ==========COMMENTS LOGIC===============*/
+
+//GET - comment form
+app.get('/books/:id/comments/new', function(req, res) {
+	//find book by id
+	Book.findById(req.params.id)
+		.then(function(foundBook) {
+			res.render('comments/new', {book: foundBook});
+		})
+		.catch(function(err) {
+			if(err) {
+				console.log(err);
+				res.redirect("/books");
+			}
+		});
+});
+
+//CREATE - new comment
+app.post('/books/:id/comments', function(req, res) {
+	Book.findById(req.params.id)
+		.then(function(book) {
+			Comment.create(req.body.comment)
+				.then(function(comment) {
+					//associate comment with username and id
+					comment.owner.id = req.user._id;
+					comment.owner.username = req.user.username;
+					//save comment and push to book
+					comment.save();
+					book.comments.push(comment);
+					//save book and redirect
+					book.save();
+					res.redirect('/books/' + book._id)
+				})
+				.catch(function(err) {
+					if(err) {
+						console.log(err);
+						res.redirect('/books');
+					}
+				})
+		})
+		.catch(function(err) {
+			if(err) {
+				console.log(err);
+				res.redirect('/books');
+			}
+		});
+});
+
+//EDIT comment
+app.get('/books/:id/comments/:comment_id/edit', function(req, res) {
+	Comment.findById(req.params.comment_id)
+		.then(function(comment) {
+			res.render('comments/edit', {book_id: req.params.id, comment: comment});
+		})
+		.catch(function(err) {
+			console.log(err);
+			res.redirect('back');
+		});
+});
+
+//UPDATE comment
+app.put('/books/:id/comments/:comment_id', function(req, res) {
+	//find comment and update
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment)
+		.then(function(updatedComment) {
+			res.redirect('/books/' + req.params.id)
+		})
+		.catch(function(err) {
+			console.log(err);
+			res.redirect('back');
+		});
+});
+
+//DELETE COMMENT
+app.delete('/books/:id/comments/:comment_id', function(req, res) {
+	//find comment by ID and delete
+	Comment.findByIdAndRemove(req.params.comment_id)
+		.then(function(comment) {
+			res.redirect('/books/' + req.params.id);
+		})
+		.catch(function(err) {
+			console.log(err);
+			res.redirect('back');
+		})
+})
+// //isLoggedIn Middleware
+// function isLoggedIn(req, res, next) {
+// 	//if logged in, move on
+// 	if (req.isAuthenticated()) {
+// 		return next();
+// 	}
+// 	//if not logged in
+// 	req.flash('error', 'You need to be logged in to do that.');
+// 	req.session.redirectTo = req.originalUrl;
+// 	res.redirect('/login');
+// }
 
 
 
