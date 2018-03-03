8daf22e9e1ab69180db19a4cd3ae5be11a79b630
Zi Nguyen <zinguyenn@gmail.com>
2018-03-03 11:21:29 -0500

    add avatar and desc

--- models/user.js
+++ models/user.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     username: String,
-    password: String
+    password: String,
+    fullname: String,
+    avatar: String,
+    desc: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
