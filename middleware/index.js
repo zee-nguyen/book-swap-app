c767c22936d55f411b1920dd01d6ee7c36ed860c
Zi Nguyen <zinguyenn@gmail.com>
2018-03-13 14:24:29 -0400

    update middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,54 +1,62 @@
-const Book 		= require("../models/book"),
-			Comment = require("../models/comment");
+const Book = require("../models/book"),
+  Comment = require("../models/comment");
 
 var middlewareObj = {
-	isLoggedIn: function (req, res, next) {
-		//if logged in, move on
-		if (req.isAuthenticated()) {
-			return next();
-		}
-		//if not logged in
-		console.log("not logged in");
-		// req.session.redirectTo = req.originalUrl;
-		res.redirect('/login');
-	},
-	checkBookOwnership: function (req, res, next) {
-		//is user logged in?
-		if(req.isAuthenticated()) {
-			//does the book belong to the user?
-			Book.findById(req.params.id, function(err, foundBook) {
-				if (err) {
-					console.log(err);
-					res.redirect('back');
-				} else {
-					if (foundBook.owner.id.equals(req.user._id)) {
-						next();
-					} else {
-						console.log("no permission to edit book");
-						res.redirect('back');
-					}
-				}
-			});
-		}
-	},
-	checkCommentOwnership: function (req, res, next) {
-			//is user logged in
-			if(req.isAuthenticated()) {
-				Comment.findById(req.params.comment_id, function(err, foundComment) {
-					if (err) {
-						console.log(err);
-						res.redirect('back');
-					} else {
-						if (foundComment.owner.id.equals(req.user._id)) {
-							next();
-						} else {
-							console.log('no permission to edit comment');
-							res.redirect('back');
-						}
-					}
-				});
-			};
-	}
+  isLoggedIn: function(req, res, next) {
+    //if logged in, move on
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    //if not logged in
+    req.flash("error", "You need to be logged in to do that.");
+    req.session.redirectTo = req.originalUrl;
+    res.redirect("/login");
+  },
+  checkBookOwnership: function(req, res, next) {
+    //is user logged in?
+    if (req.isAuthenticated()) {
+      //does the book belong to the user?
+      Book.findById(req.params.id, (err, foundBook) => {
+        if (err) {
+          req.flash(
+            "error",
+            "Oops! Something goes wrong. Please try again in a few minutes."
+          );
+          // console.log(err);
+          res.redirect("back");
+        } else {
+          if (foundBook.owner.id.equals(req.user._id)) {
+            next();
+          } else {
+            req.flash("error", "You don't have permission to do that.");
+            res.redirect("back");
+          }
+        }
+      });
+    }
+  },
+  checkCommentOwnership: function(req, res, next) {
+    //is user logged in
+    if (req.isAuthenticated()) {
+      Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) {
+          req.flash(
+            "error",
+            "Oops! Something goes wrong. Please try again in a few minutes."
+          );
+          // console.log(err);
+          res.redirect("back");
+        } else {
+          if (foundComment.owner.id.equals(req.user._id)) {
+            next();
+          } else {
+            req.flash("error", "You don't have permission to do that.");
+            res.redirect("back");
+          }
+        }
+      });
+    }
+  }
 };
 
 module.exports = middlewareObj;
